704549f5dd4742f168847458b2b8958a2d3a5c0a
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-01-11 08:48:53 +0000

    completed BetterLinearTest class

--- AlgorytmySortowania/src/searchingalgorithms/betterlinear/BetterLinearTest.java
+++ AlgorytmySortowania/src/searchingalgorithms/betterlinear/BetterLinearTest.java
@@ -1,38 +1,35 @@
 package betterlinear;
 
+import logging.ArrayLogger;
+import logging.ValueLogger;
+import testdata.TestDataGenerator;
+
 public class BetterLinearTest {
 
 	public static void main(String[] args) {
+
+		// Generates list of random values.
+		TestDataGenerator generator = new TestDataGenerator();
+		int[] somelist = generator.generateRandomNumbers(20, 100);
+
+		// Generates a random value.
+		int value = generator.generateRandomValue(100);
+
+		// Prints random list.
+		System.out.print("Searched list: ");
+		ArrayLogger.log(somelist);
+
+		// Prints searched value.
+		ValueLogger.logValue(value);
+
+		BetterLinear next = new BetterLinear();
+		int answer = next.searchBetterLinearly(somelist, value);
 		
-		int Lista[] = new int[11];
-		Lista[0] = 15;
-		Lista[1] = 23;
-		Lista[2] = 24;
-		Lista[3] = 125;
-		Lista[4] = 18;
-		Lista[5] = 56;
-		Lista[6] = 2;
-		Lista[7] = 24;
-		Lista[8] = 35;
-		Lista[9] = 24;
-		Lista[10] = 17;
-		
-		int wyszukiwanaWartosc = 17;
-		int odpowiedz = -1;
-		
-		//Wyszukuje pierwsz¹ wyszukiwan¹Wartosc znajduj¹c¹ siê na liœcie.
-		//Zaprzestaje przeszukiwania ca³ej listy jeœli wyszukiwanaWartosc zostanie odnaleziona(funkcja break).
-		for (int i=0; i<Lista.length; i++) {
-			if (Lista[i] == wyszukiwanaWartosc) {
-				odpowiedz = i;
-				break;
-			}
-		}
-		
-		if (odpowiedz >= 0) {
-			System.out.print("Wyszukiwana wartosc znajduje sie pod indeksem nr: " + odpowiedz);
-		}else {
-			System.out.print("Wyszukiwana wartosc nie znajduje sie na liscie.");
+		// Prints result of search.
+		if (answer >= 0) {
+			System.out.print("Searched value position number:  " + answer);
+		} else {
+			System.out.print("Searched value is not on the list.");
 		}
 
 	}
