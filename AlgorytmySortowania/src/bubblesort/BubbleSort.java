d472f6d679739171d272c5ccb7d8940fc48f84a0
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-01-11 11:34:30 +0000

    working bubblesort

--- AlgorytmySortowania/src/bubblesort/BubbleSort.java
+++ AlgorytmySortowania/src/bubblesort/BubbleSort.java
@@ -2,4 +2,17 @@ package bubblesort;
 
 public class BubbleSort {
 
+	public void sortBubbly(int[] array) {
+		int t;
+		
+		for (int a = 1; a < array.length; a++) {
+			for (int b = array.length - 1; b >= a; b--) {
+				if (array[b - 1] > array[b]) {
+					t = array[b - 1];
+					array[b - 1] = array[b];
+					array[b] = t;
+				}
+			}
+		}
+	}
 }
--- AlgorytmySortowania/src/bubblesort/BubbleSortTest.java
+++ AlgorytmySortowania/src/bubblesort/BubbleSortTest.java
@@ -1,10 +1,25 @@
 package bubblesort;
 
+import logging.ArrayLogger;
+import testdata.TestDataGenerator;
+
 public class BubbleSortTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		TestDataGenerator generator = new TestDataGenerator();
+		int[] newlist = generator.generateRandomNumbers(10, 100);
+
+		// Prints list before sorting.
+		System.out.print("List before sorting: ");
+		ArrayLogger.log(newlist);
+
+		BubbleSort sort = new BubbleSort();
+		sort.sortBubbly(newlist);
+		
+		// Prints sorted list.
+		System.out.print("Sorted list: ");
+		ArrayLogger.log(newlist);
 	}
 
 }
