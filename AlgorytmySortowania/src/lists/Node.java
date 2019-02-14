dc6f3d179104057e2169773ee0f9c8f51be219bd
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-02-14 11:15:42 +0000

    tried to use ternary operator on hasNext method in class Node

--- AlgorytmySortowania/src/lists/Node.java
+++ AlgorytmySortowania/src/lists/Node.java
@@ -1,23 +1,43 @@
 package lists;
 
 public class Node {
-	
-	private int value;
-	private Node next;
-	
+
+	int value;
+	Node next;
+
+	public Node() {
+
+	}
+
+	public Node(int value) {
+		this.value = value;
+	}
+
 	public int getValue() {
 		return value;
 	}
+
 	public void setValue(int value) {
 		this.value = value;
 	}
+
 	public Node getNext() {
 		return next;
 	}
+
 	public void setNext(Node next) {
 		this.next = next;
 	}
 
-	
-	
+	public boolean hasNext() { 
+		//this.next != null ? true : false;
+		
+		if(this.next != null) { //zmieniæ drabinkê if - else
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
 }
