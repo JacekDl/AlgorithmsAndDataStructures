5b0aa178aa8bb0a81d2543b5e29fa1e4f9741a02
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-02-22 11:57:44 +0000

    current version of LinkedListTester class

--- AlgorytmySortowania/src/lists/LinkedListTester.java
+++ AlgorytmySortowania/src/lists/LinkedListTester.java
@@ -1,5 +1,31 @@
 package lists;
 
 public class LinkedListTester {
+	public static void main(String[] args) {
+	
+		LinkedList nextList = new LinkedList();
+		nextList.insert(0, 11);
+		nextList.add(11);
+		nextList.add(11);
+		nextList.add(11);
+		nextList.add(11);
+		nextList.add(11);
+		nextList.insert(2, 12);
+		nextList.insert(2, 13);
+		nextList.insert(0, 14);
+		nextList.insert(0, 15);
 
+		nextList.print();
+		System.out.println("LinkedList size is: " + nextList.size());
+		
+		int[] arrayList = new int[] {11,22,23};
+		nextList.add(arrayList);
+		nextList.print();
+		System.out.println("LinkedList size is: " + nextList.size());
+		
+		nextList.removeAll(11);
+		nextList.print();
+		System.out.println("LinkedList size is: " + nextList.size());
+		
+	}
 }
