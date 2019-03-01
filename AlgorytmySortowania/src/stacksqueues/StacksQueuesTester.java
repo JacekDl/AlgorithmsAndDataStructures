fc22f0efb6146f78bb8881595625ebd399e45921
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-03-01 14:16:00 +0000

    end of day changes to Node class and StacksQueuesTester

--- AlgorytmySortowania/src/stacksqueues/StacksQueuesTester.java
+++ AlgorytmySortowania/src/stacksqueues/StacksQueuesTester.java
@@ -3,31 +3,48 @@ package stacksqueues;
 public class StacksQueuesTester {
 
 	public static void main(String[] args) {
-		/*
-		 * Stack<Integer> myStack = new StackImplWithList<>();
-		 * 
-		 * //test 0: check the value at the top if stack is empty:
-		 * System.out.println("Test 0: "); myStack.peek();
-		 * 
-		 * //test 1: add values to the stack System.out.println("Test 1: ");
-		 * myStack.push(11); myStack.push(12); myStack.push(13); myStack.push(14);
-		 * myStack.print();
-		 * 
-		 * //test 2: check the value at the top System.out.println("Test 2: ");
-		 * myStack.peek();
-		 * 
-		 * //test 3: remove value from the stack System.out.println("Test 3: ");
-		 * myStack.pop(); myStack.print();
-		 * 
-		 * //test 4 remove last value from the stack: System.out.println("Test 4: ");
-		 * myStack.pop(); myStack.pop(); myStack.pop(); myStack.pop(); myStack.print();
-		 * 
-		 * //test 5: create and manipulate stack type String
-		 * System.out.println("Test 5:"); StackImplWithList<String> stringStack = new
-		 * StackImplWithList<>(); stringStack.push("Jacek"); stringStack.push("Ola");
-		 * stringStack.push("Józef"); stringStack.print(); stringStack.peek();
-		 * stringStack.pop(); stringStack.print();
-		 */
+
+		Stack<Integer> myStack = new StackImplWithList<>();
+
+		// test 0: check the value at the top if stack is empty
+		System.out.println("Test 0: ");
+		myStack.peek();
+
+		// test 1: add values to the stack 
+		System.out.println("Test 1: ");
+		myStack.push(11);
+		myStack.push(12);
+		myStack.push(13);
+		myStack.push(14);
+		myStack.print();
+
+		// test 2: check the value at the top 
+		System.out.println("Test 2: ");
+		myStack.peek();
+
+		// test 3: remove value from the stack 
+		System.out.println("Test 3: ");
+		myStack.pop();
+		myStack.print();
+
+		// test 4 remove last value from the stack 
+		System.out.println("Test 4: ");
+		myStack.pop();
+		myStack.pop();
+		myStack.pop();
+		myStack.pop();
+		myStack.print();
+
+		// test 5: create and manipulate stack type String
+		System.out.println("Test 5:");
+		StackImplWithList<String> stringStack = new StackImplWithList<>();
+		stringStack.push("Jacek");
+		stringStack.push("Wacek");
+		stringStack.push("Pankracek");
+		stringStack.print();
+		stringStack.peek();
+		stringStack.pop();
+		stringStack.print();
 
 		QueueImplWithList<Integer> myQueue = new QueueImplWithList<>();
 
@@ -35,8 +52,8 @@ public class StacksQueuesTester {
 		System.out.println("Test 6: returns value from the front of Queue (end of LinkedList).");
 		System.out.println(myQueue.peek());
 		myQueue.print();
-		
-		// test 7: add values to the end of Queue (front of LinkedList) 
+
+		// test 7: add values to the end of Queue (front of LinkedList)
 		System.out.println("Test 7: add values to the Queue: ");
 		myQueue.push(21);
 		myQueue.push(22);
@@ -57,6 +74,19 @@ public class StacksQueuesTester {
 		System.out.println(myQueue.poll());
 		System.out.println(myQueue.poll());
 
+		// test 10: create and manipulate Queue using data type String
+		System.out.println("Test 10: create and manipulate Queue using data type String.");
+		QueueImplWithList<String> nameQueue = new QueueImplWithList<>();
+		nameQueue.push("Jacek");
+		nameQueue.push("Wacek");
+		nameQueue.push("Pankracek");
+		nameQueue.print();
+		System.out.println(nameQueue.peek());
+		nameQueue.print();
+		System.out.println(nameQueue.poll());
+		System.out.println(nameQueue.poll());
+		System.out.println(nameQueue.poll());
+		System.out.println(nameQueue.poll());
 	}
 
 }
