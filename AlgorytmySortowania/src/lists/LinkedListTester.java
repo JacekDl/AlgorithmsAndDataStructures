8b5861c5ebca49f78ecbdc99dabbad096cf697ad
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-03-07 11:12:16 +0000

    Test organization

--- AlgorytmySortowania/src/lists/LinkedListTester.java
+++ AlgorytmySortowania/src/lists/LinkedListTester.java
@@ -2,7 +2,9 @@ package lists;
 
 public class LinkedListTester {
 	public static void main(String[] args) {
-	
+
+		// test 1: create LinkedList object and add some values:
+		System.out.println("Test 1: create LinkedList object and add some values:");
 		LinkedList<Integer> nextList = new LinkedList<Integer>();
 		nextList.add(11);
 		nextList.add(12);
@@ -10,101 +12,111 @@ public class LinkedListTester {
 		nextList.add(14);
 		nextList.add(15);
 		nextList.add(16);
-
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
+
+		// test 2: remove last value:
+		System.out.println("Test 2: remove last value: ");
 		nextList.removeLast();
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
+
+		// test 3: remove first value:
+		System.out.println("Test 3: remove first value:");
 		nextList.removeFirst();
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
+
+		// test 4: remove specific value:
+		System.out.println("Test 4: remove specific value(12):");
 		nextList.remove(12);
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
+
+		// test 5: insert value at the beginning:
+		System.out.println("Test 5: insert value at the beginning:");
 		nextList.insert(0, 12);
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
-//		int[] intList = new int[] {11,11,11,21,22,23}; //TODO how to create generic list?
-//		nextList.add(intList);
-//		nextList.add(11);
-//		nextList.print();
-//		System.out.println("LinkedList size is: " + nextList.size());
-		
+
+		// test 6: insert values in the specific position:
+		System.out.println("Test 6: insert values in the specific position:");
+		nextList.insert(0, 11);
+		nextList.insert(0, 11);
+		nextList.insert(4, 11);
+		nextList.print();
+
+		// test 7: add list to LinkedList:
+		System.out.println("Test 7: add list to LinkedList:");
+		Integer[] intList = new Integer[] { 11, 11, 11, 21, 22, 23 }; // TODO how to create generic list? ANSWER: you
+																		// cannot create generic list!
+		nextList.add(intList);
+		nextList.print();
+
+		// test 8: remove all values(11):
+		System.out.println("Test 8: remove all values:");
 		nextList.removeAll(11);
 		nextList.print();
-		System.out.println("LinkedList size is: " + nextList.size());
-		
-		///////////
-		/*LinkedList<Character> charList = new LinkedList<Character>();
+		System.out.println();
+
+		// test 9: create and manipulate LinkedList of type Character:
+		System.out.println("Test 9: create and manipulate LinkedList of type Character:");
+		LinkedList<Character> charList = new LinkedList<Character>();
 		charList.add('a');
 		charList.add('b');
 		charList.add('c');
 		charList.add('d');
 		charList.add('e');
 		charList.add('f');
-
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());
-		
+
+		System.out.println("Remove last element:");
 		charList.removeLast();
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());
-		
+
+		System.out.println("Remove first element:");
 		charList.removeFirst();
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());
-		
+
+		System.out.println("Remove specific element(b):");
 		charList.remove('b');
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());
-		
-		charList.insert(0, 'c');
+
+		System.out.println("Add element to the front: ");
+		charList.insert(0, 'b');
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());
-		
-//		char[] nextArrayList = new char[] {'a','a','a','k','l','m'}; //TODO how to create generic list?
-//		charList.add(nextArrayList);
-//		charList.add('a');
-//		charList.print();
-//		System.out.println("LinkedList size is: " + charList.size());
-		
-		charList.removeAll('c');
+
+		System.out.println("Insert element to the middle: ");
+		charList.insert(2, 'b');
 		charList.print();
-		System.out.println("LinkedList size is: " + charList.size());*/
-		
+
+		System.out.println("Add array:");
+		Character[] nextArrayList = new Character[] { 'b', 'b', 'b', 'k', 'l', 'm' };
+		charList.add(nextArrayList);
+		charList.print();
+
+		System.out.println("Remove all elements of specific value: ");
+		charList.removeAll('b');
+		charList.print();
+
 		LinkedList<String> nameList = new LinkedList<String>();
 		nameList.add("Jacek");
-		nameList.add("Ola");
-		nameList.add("Józef");
+		nameList.add("Wacek");
+		nameList.add("Pankracek");
 		nameList.print();
-		System.out.println("LinkedList size is: " + nameList.size());
-		
-		nameList.removeFirst();
-		nameList.print();
-		System.out.println("LinkedList size is: " + nameList.size());
-		
+
 		nameList.insert(1, "Jacek");
 		nameList.insert(1, "Jacek");
 		nameList.insert(4, "Jacek");
 		nameList.print();
-		System.out.println("LinkedList size is: " + nameList.size());
-		
+
+		String[] names = new String[] { "Ola", "Ola", "Ola" };
+		nameList.add(names);
+		nameList.print();
+
 		nameList.removeAll("Jacek");
 		nameList.print();
-		System.out.println("LinkedList size is: " + nameList.size());
-		
-		/*Node myNode = new Node(15);
-		System.out.println(myNode.getValue());
-		myNode.setValue(16);
-		System.out.println(myNode.getValue());
-		Node nextNode = new Node(17);
-		myNode.setNext(nextNode);
-		System.out.println(myNode.getValue() + " " +nextNode.getValue());*/
+
+		/*
+		 * Node myNode = new Node(15); System.out.println(myNode.getValue());
+		 * myNode.setValue(16); System.out.println(myNode.getValue()); Node nextNode =
+		 * new Node(17); myNode.setNext(nextNode); System.out.println(myNode.getValue()
+		 * + " " +nextNode.getValue());
+		 */
 	}
 }
