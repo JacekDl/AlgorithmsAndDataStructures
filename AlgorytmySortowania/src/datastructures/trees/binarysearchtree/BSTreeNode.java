a5b22a19be17a702616d48a9c7ae05d5ffaf6e10
Jacek Dlugopolski <34134192+JacekDl@users.noreply.github.com>
2019-09-10 11:30:44 +0200

    added field BSTreeNode parent to BSTreeNode class

--- AlgorytmySortowania/src/datastructures/trees/binarysearchtree/BSTreeNode.java
+++ AlgorytmySortowania/src/datastructures/trees/binarysearchtree/BSTreeNode.java
@@ -5,28 +5,43 @@ public class BSTreeNode {
 	private int value;
 	private BSTreeNode left;
 	private BSTreeNode right;
+	private BSTreeNode parent;
 	
 	//constructor:
 	public BSTreeNode(int value) {
 		this.value = value;
 	}
-	
+
+	//int
 	public int getValue() {
 		return value;
 	}
+	
 	public void setValue(int value) {
 		this.value = value;
 	}
+	
 	public BSTreeNode getLeft() {
 		return left;
 	}
+	
 	public void setLeft(BSTreeNode left) {
 		this.left = left;
 	}
+	
 	public BSTreeNode getRight() {
 		return right;
 	}
+	
 	public void setRight(BSTreeNode right) {
 		this.right = right;
+	}
+	
+	public BSTreeNode getParent() {
+		return parent;
+	}
+
+	public void setParent(BSTreeNode parent) {
+		this.parent = parent;
 	}
 }
